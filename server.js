1fc5d9117539100b63982528e7b777b8ce34d6dc
raedeid <raededialrfou12345@gmail.com>
2019-12-23 17:51:19 +0200

    solve half of lab

---
+++ server.js
@@ -0,0 +1,150 @@
+'use strict';
+// Express
+const express = require('express');
+
+const superagent = require('superagent');
+
+// initialize a server
+const server = express();
+
+
+// Cross Origin Resource Sharing
+const cors = require('cors');
+server.use(cors()); // give access
+
+// get all environment variable you need
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+const EVENT_FUL_KEY = process.env.EVENT_FUL_KEY;
+
+
+// Make the app listening
+server.listen(PORT, () => console.log('Listening at port 3000'));
+
+
+
+server.get('/', (request, response) => {
+    response.status(200).send('App is working CLAAAAASS');
+});
+
+/* {
+    "search_query": "lynwood",
+    "formatted_query": "lynood,... ,WA, USA",
+    "latitude": "47.606210",
+    "longitude": "-122.332071"
+  }
+*/
+
+
+server.get('/location', locationHandler);
+
+function Location(city, locationData) {
+    this.formatted_query = locationData[0].display_name;
+    this.latitude = locationData[0].lat;
+    this.longitude = locationData[0].lon;
+    this.search_query = city;
+}
+
+function locationHandler(request, response) {
+    // Read the city from the user (request) and respond
+    let city = request.query['city'];
+    getLocationData(city)
+        .then((data) => {
+            response.status(200).send(data);
+        });
+}
+function getLocationData(city) {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+
+    // Superagent
+    return superagent.get(url)
+        .then((data) => {
+            let location = new Location(city, data.body);
+            return location;
+        });
+}
+
+
+
+server.get('/weather', weatherHandler);
+
+function Weather(day) {
+    this.time = new Date(day.time * 1000).toDateString();
+    this.forecast = day.summary;
+}
+
+function weatherHandler(request, response) {
+    let lat = request.query['latitude'];
+    console.log(lat)
+    let lng = request.query['longitude'];
+    getWeatherData(lat, lng)
+        .then((data) => {
+            response.status(200).send(data);
+        });
+
+}
+
+function getWeatherData(lat, lng) {
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${lng}`;
+    return superagent.get(url)
+        .then((weatherData) => {
+            console.log(weatherData.body.daily.data);
+            let weather = weatherData.body.daily.data.map((day) => new Weather(day));
+            return weather;
+        });
+}
+// [
+//     {
+//       "link": "http://seattle.eventful.com/events/seattle-code-101-explore-software-development-/E0-001-126675997-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+//       "name": "Seattle Code 101: Explore Software Development",
+//       "event_date": "Sat Dec 7 2019",
+//       "summary": "Thinking about a new career in software development? Start here! In this one-day workshop, you&#39;ll get a taste of a day in the life of a software developer. Code 101 helps you learn what it’s like to be a software developer through a day-long immersive course for beginners that focuses on front-end web development technologies. "
+//     },
+//     {
+//       "link": "http://seattle.eventful.com/events/geekzonehosting-raspberry-pi-jam-session-code-c-/E0-001-121109275-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+//       "name": "GeekZoneHosting Raspberry Pi Jam Session & Code Carnival 2019",
+//       "event_date": "Sat Dec 7 2019",
+//       "summary": "Join fellow coders, builders, and Raspberry Pi makers in an 8 hour all day event Jam Session builder and code-a-thone to celebrate computer science education week 2019."
+//     },
+//     ...
+//   ]
+function eventFulHandler(request, response) {
+    let city = request.query['city'];
+    console.log(city)
+    getEventData(city)
+        .then((data) => {
+            response.status(200).send(data);
+        });
+}
+function getEventData(city) {
+    const url = `http://api.eventful.com/json/events/search?app_key=${EVENT_FUL_KEY}&q=${city}&format=json&limit=1`;
+    console.log(url)
+    return superagent.get(url)
+        .then((element) => {
+            // console.log(element);
+            // let weather = weatherData.body.daily.data.map((day) => new Weather(day));
+            return element.text;
+        })
+}
+server.get('/eventful', eventFulHandler);
+
+// function Event(event){
+//     this.link =;
+//     this.name =;
+//     this.event_date=;
+//     this.summary=;
+// }
+
+
+server.use('*', (request, response) => {
+    response.status(404).send('Sorry, not found');
+});
+
+server.use((error, request, response) => {
+    response.status(500).send(error);
+});
+
+
+
